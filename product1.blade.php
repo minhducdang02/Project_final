35be11d0e1f78808bafb856badeac90f974aaa5d
minhducdang02 <132746232+minhducdang02@users.noreply.github.com>
2023-08-17 01:12:55 +0700

    Add files via upload

---
+++ product1.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/product1.css') }}">
+
+</head>
+
+<body>
+    <div class="section1">
+        <div class="user">
+            <p>2</p>
+            <p>Dat</p>
+        </div>
+        <div class="info">
+            <div class="date">
+                <p>date</p>
+                <p>23/2002</p>
+            </div>
+            <div class="no">
+                <p>order no</p>
+                <p>299-452345432</p>
+            </div>
+            <div class="status">
+                <p>staut</p>
+                <p>prev</p>
+            </div>
+            <div class="delivery">
+                <p>address</p>
+                <p>20003,newyor</p>
+            </div>
+        </div>
+        <div class="product">
+            <div class="product-detail">
+                <img src="" alt="">
+                <div class="product-name">
+                    <p>name</p>
+                    <p>quantity</p>
+                    <p>color</p>
+                </div>
+            </div>
+        </div>
+        <div class="price">
+            <p>39$</p>
+        </div>
+    </div>
+    <div class="section2">
+        <p>payment</p>
+    </div>
+</body>
+
+</html>
